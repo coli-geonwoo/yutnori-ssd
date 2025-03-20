0d2982208434537b6c59605c1bf043c8e316d7a7
김건우 <kkwoo001021@naver.com>
2025-03-20 13:40:12 +0900

    feat: 예시코드 추가 (#2)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("예시 코드 추가");
         System.out.println("Hello, World!");
     }
 }
