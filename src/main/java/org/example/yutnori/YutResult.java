73b90c5bf926d9b99ad4b926bf7cce895ddece0d
coli <kkwoo001021@naver.com>
2025-03-21 17:47:46 +0900

    feat: YutResult 도메인 추가

---
+++ src/main/java/org/example/yutnori/YutResult.java
@@ -0,0 +1,20 @@
+package org.example.yutnori;
+
+public enum YutResult {
+
+    DO(1),
+    GAE(2),
+    GUL(3),
+    YUT(4),
+    MO(5);
+
+    private final int step;
+
+    YutResult(int step) {
+        this.step = step;
+    }
+
+    public int getStep() {
+        return step;
+    }
+}
