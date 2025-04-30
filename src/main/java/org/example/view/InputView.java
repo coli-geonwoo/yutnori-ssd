5d9532eab03e269b0622fe53351d21d760f89700
coli <kkwoo001021@naver.com>
2025-05-01 07:52:38 +0900

    feat: view 로직 구현

---
+++ src/main/java/org/example/view/InputView.java
@@ -0,0 +1,32 @@
+package org.example.view;
+
+import java.util.List;
+import org.example.domain.piece.GamePieces;
+import org.example.domain.yut.YutResult;
+import org.example.dto.GameInitializeDto;
+import org.example.dto.YutGenerationRequest;
+
+public interface InputView {
+
+    //팀 개수, 말개수, 보드 유형 받기
+    GameInitializeDto readInitializeInfo();
+
+    //윷 던지기 요청 정보 읽기 (옵션 / 지정 윷의 경우, 윷 정보)
+    YutGenerationRequest readYutGenerationInfo();
+
+    //나의 말 중에 어떤 걸 이동시킬 것인지 gamePieces 반환
+    GamePieces readMovingPiece(List<GamePieces> myGamePieces);
+
+    //여러개의 윷 결과 중에 무엇을 적용할 것인지
+    YutResult chooseYutResult(List<YutResult> yutResults);
+
+    // 이동가능한 칸 중에 어디로 이동할 것인지 -> place 이름 반환
+    String chooseMovingPlace(List<String> movablePlaces);
+
+    //여러개의 잡을 수 있는 말 중에 잡을 말 고르기 -> 잡을 말의 PieceId반환
+    GamePieces readCatchingPiece(List<GamePieces> catchAblePieces);
+
+    // 여러개의 업을 수 있는 말 중에 업을 말 고르기 -> 업을 말의 PieceId반환
+    // 업지 않는 선택을 했다면 null 반환
+    GamePieces readGroupingPiece(List<GamePieces> catchAblePieces);
+}
